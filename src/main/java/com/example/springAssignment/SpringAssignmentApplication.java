0780fa254ce5a21436d00fbcd2d2564d4cb7885b
longton95 <longton95@gmail.com>
2017-12-28 05:06:15 +0000

    Removed commandLineRunner for hosting

--- src/main/java/com/example/springAssignment/SpringAssignmentApplication.java
+++ src/main/java/com/example/springAssignment/SpringAssignmentApplication.java
@@ -10,42 +10,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-public class SpringAssignmentApplication implements CommandLineRunner {
+public class SpringAssignmentApplication {
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(SpringAssignmentApplication.class, args);
 	}
 
-	@Autowired
-	private UserRepository repository;
 
-	public void run(String... args) throws Exception {
-
-		repository.deleteAll();
-
-		repository.save(new User("Josh", "Longton", 1, 2, 3));
-		repository.save(new User("Samir", "Mohammed", 1, 2, 3));
-		repository.save(new User("John", "Doe",  1, 2, 3));
-
-		System.out.println("User's found with findAll():");
-		System.out.println("-------------------------------");
-		for (User user : repository.findAll()) {
-			System.out.println(user);
-		}
-		System.out.println();
-
-		System.out.println("User found with findByFirstName('John'):");
-		System.out.println("--------------------------------");
-		System.out.println(repository.findByFirstName("John"));
-		System.out.println();
-
-		System.out.println("User found with findByLastName('Longton'):");
-		System.out.println("--------------------------------");
-		for (User user : repository.findByLastName("Longton")) {
-			System.out.println(user);
-		}
-
-
-	}
 }
