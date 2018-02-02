66e8a812ba3cdbd2b810a1dc8a0cdbfdd31d4b25
longton95 <longton95@gmail.com>
2018-02-02 10:30:32 +0000

    Created users on start

--- src/main/java/com/example/springAssignment/SpringAssignmentApplication.java
+++ src/main/java/com/example/springAssignment/SpringAssignmentApplication.java
@@ -1,8 +1,11 @@
 package com.example.springAssignment;
 
+import com.example.springAssignment.domain.User;
+import com.example.springAssignment.domain.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
@@ -13,5 +16,20 @@ public class SpringAssignmentApplication {
 		SpringApplication.run(SpringAssignmentApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner init(UserRepository userRepository) {
+
+		return args -> {
+			userRepository.deleteAll();
+			userRepository.save(new User("Josh", "Longton","admin","longton95@gmail.com", 1, 2, 3));
+			userRepository.save(new User("Samir", "Mohammed","user","samir@gmail.com", 1, 2, 3));
+			userRepository.save(new User("John", "Doe","user","john@gmail.com", 1, 2, 3));
+
+			System.out.println(userRepository.findUserByEmail("longton95@gmail.com"));
+
+
+		};
+
+	}
 
 }
