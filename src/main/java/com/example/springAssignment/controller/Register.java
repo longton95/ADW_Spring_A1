cb39c4afa48b954bd819b6d6179fb7216b7489f3
longton95 <longton95@gmail.com>
2018-02-02 10:27:40 +0000

    Linked model to template in register

--- src/main/java/com/example/springAssignment/controller/Register.java
+++ src/main/java/com/example/springAssignment/controller/Register.java
@@ -1,14 +1,19 @@
 package com.example.springAssignment.controller;
 
+import com.example.springAssignment.domain.User;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class Register {
     private static final String REGISTER = "register";
 
 
     @GetMapping(value = "/register")
-    public String register () {
+    public String register (Model model) {
+
+        model.addAttribute ("user", new User());
 
         return REGISTER;
 
