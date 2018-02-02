40b2a88529a016b42afa6de46690753a6aecd4ba
longton95 <longton95@gmail.com>
2018-02-02 10:23:42 +0000

    Updated Add post mapping so the user is passed in the form not the url

--- src/main/java/com/example/springAssignment/controller/Add.java
+++ src/main/java/com/example/springAssignment/controller/Add.java
@@ -1,13 +1,14 @@
 package com.example.springAssignment.controller;
 
 import com.example.springAssignment.service.UserService;
-import org.apache.tomcat.jni.User;
+import com.example.springAssignment.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
@@ -18,34 +19,47 @@ public class Add {
     private UserService userService;
 
     private static final String ADD = "user/add";
-    private static final String INDEX = "user/index";
-
-
-
+    private static final String INDEX = "index";
 
 
     @GetMapping(value = "profile/add")
-    public String setUser (Model model) {
+    public String getUser (Model model) {
 
-        model.addAttribute ("entries", this.userService.findAllEntries ());
-        model.addAttribute ("newEntry", new User());
+//        model.addAttribute ("entries", this.userService.findAllEntries ());
+        model.addAttribute ("newData", new User());
 
         return ADD;
 
     }
-    @PostMapping(value = "/")
-    public String addComment (Model model,
-                              @Valid @ModelAttribute("entries") UserService newData, BindingResult bindingResult) {
+
+    @PostMapping (value = "profile/add")
+    public String update (Model model,
+//                               @PathVariable String email,
+                               @Valid @ModelAttribute ("newData") User newData,
+
+                             BindingResult bindingResult) {
+
 
         if (bindingResult.hasErrors ()) {
+            System.out.println("supppppp");
+            System.out.println(bindingResult.getAllErrors());
+
             model.addAttribute ("entries", this.userService.findAllEntries ());
 
             return ADD;
         }
         else {
-            this.userService.save(newData);
 
-            return INDEX;
+            User current = this.userService.findUserByEmail ("john@gmail.com");
+
+            current.setBitcoin (newData.getBitcoin ());
+            current.setEtherium (newData.getEtherium ());
+            current.setLitecoin (newData.getLitecoin ());
+
+            this.userService.save (current);
+
+            return "redirect:/";
         }
     }
+//
 }
